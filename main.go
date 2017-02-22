eecec664a41ca2ca77d0fa82bfb7e2c091c576a0
oleg.sobchuk <oleg.sobchuk@mev.com>
2017-02-22 13:55:35 +0200

    Add contact details

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ type Todo struct {
 	Done bool
 }
 
+type Contact struct {
+	Email   string
+	Subject string
+	Message string
+}
+
 func main() {
 	app := iris.New(iris.Configuration{Gzip: false, Charset: "UTF-8"})
 
@@ -18,14 +24,39 @@ func main() {
 	app.Adapt(httprouter.New())
 	app.Adapt(view.HTML("./app/views/", ".html"))
 
+	// todos
 	todos := []Todo{
 		{"Initialize application", true},
 		{"Upload to github", true},
 		{"Make nice template", false},
 	}
 
-	app.Get("/", func(ctx *iris.Context) {
-		ctx.Render("test.html", struct{ Todos []Todo }{todos})
+	app.Get("/todo", func(ctx *iris.Context) {
+		ctx.Render("todo.html", struct{ Todos []Todo }{todos})
 	})
+
+	// contact_details
+	app.Get("/contact_detail", func(ctx *iris.Context) {
+		ctx.Render("contact_detail.html", nil)
+	})
+
+	app.Post("/contact_detail", func(ctx *iris.Context) {
+		// contact := Contact{
+		// 	Email:   ctx.FormValue("email"),
+		// 	Subject: ctx.FormValue("subject"),
+		// 	Message: ctx.FormValue("message"),
+		// }
+
+		var contact Contact
+		ctx.ReadForm(&contact)
+		var emailPresent bool
+		if contact.Email == "" {
+			emailPresent = false
+		} else {
+			emailPresent = true
+		}
+		ctx.Render("contact_detail.html", struct{ Success bool }{emailPresent})
+	})
+
 	app.Listen(":8080")
 }
